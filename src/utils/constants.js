4094e07d1dee662427564be27a58cae5dff2f1f4
Anjaney Tambat <anjaneytambat72@gmail.com>
2026-01-01 12:52:46 +0530

    fix: remove trailing slash in API_BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://food-delivery-app-gfw8.onrender.com/" 
+    ? "https://food-delivery-app-gfw8.onrender.com" 
     : "http://localhost:5000";
 
 export const CDN_URL = 
